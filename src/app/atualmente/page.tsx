f711b76da69bae2a81683182199217f686264f09
ericmeneg <ericvinicius.menegon@gmail.com>
2024-11-27 20:37:33 -0300

    Começando a criação da página de modelos matemáticos, temporariamente desativando o banner da página atualmente para permitir deploy no render

--- src/app/atualmente/page.tsx
+++ src/app/atualmente/page.tsx
@@ -5,76 +5,76 @@ import Titulo from "@/components/titulo";
 import Footer from "@/components/footer";
 import React, { useState, useEffect } from "react";
 
-interface dataType {
-    nivel: number,
-    data_iniSE: number,
-    casos: number
-}
+// interface dataType {
+//     nivel: number,
+//     data_iniSE: number,
+//     casos: number
+// }
 
 export default function Atualmente() {
-    const [status, setStatus] = useState< number | null | undefined >(null)
-    const [data, setData] = useState< dataType[] | null | undefined >(null)
-    const [loading, setLoading] = useState< boolean>(true)
-    const [error, setError] = useState< string | null >(null)
+    // const [status, setStatus] = useState< number | null | undefined >(null)
+    // const [data, setData] = useState< dataType[] | null | undefined >(null)
+    // const [loading, setLoading] = useState< boolean>(true)
+    // const [error, setError] = useState< string | null >(null)
 
-    useEffect(()=>{
-        const fetchData = async () => {
-            try {
-                const response = await fetch("https://pidengue2backend.onrender.com/proxy/?url=https%3A%2F%2Finfo.dengue.mat.br%2Fapi%2Falertcity%2F%3Fgeocode%3D3520509%26disease%3Ddengue%26format%3Djson%26ew_start%3D08%26ey_start%3D2008%26ew_end%3D25%26ey_end%3D2029%27")
-                if (!response.ok){
-                    throw new Error("Não foi possível obter uma resposta")
-                }
-                const jsonData = await response.json()
-                setData(jsonData)
-                setStatus(data?.[0]?.nivel)
-            } catch (error: unknown){
-                if (error instanceof Error){
-                    setError(error.message)
-                } else {
-                    setError('Um erro desconhecido ocorreu')
-                }
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchData()
-    }, [])
+    // useEffect(()=>{
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch("https://pidengue2backend.onrender.com/proxy/?url=https%3A%2F%2Finfo.dengue.mat.br%2Fapi%2Falertcity%2F%3Fgeocode%3D3520509%26disease%3Ddengue%26format%3Djson%26ew_start%3D08%26ey_start%3D2008%26ew_end%3D25%26ey_end%3D2029%27")
+    //             if (!response.ok){
+    //                 throw new Error("Não foi possível obter uma resposta")
+    //             }
+    //             const jsonData = await response.json()
+    //             setData(jsonData)
+    //             setStatus(data?.[0]?.nivel)
+    //         } catch (error: unknown){
+    //             if (error instanceof Error){
+    //                 setError(error.message)
+    //             } else {
+    //                 setError('Um erro desconhecido ocorreu')
+    //             }
+    //         } finally {
+    //             setLoading(false)
+    //         }
+    //     }
+    //     fetchData()
+    // }, [])
 
 
-    let classeStatus = ''
-    let statusStringCor = ''
-    if (status == 1){
-        classeStatus = 'text-lime-500'
-        statusStringCor = 'verde'
-    } else if (status == 2){
-        classeStatus = 'text-amber-300'
-        statusStringCor = 'amarelo'
-    } else if (status == 3) {
-        classeStatus = 'text-amber-500'
-        statusStringCor = 'laranja'
-    } else {
-        classeStatus = 'text-red-500'
-        statusStringCor = 'vermelho'
-    }
+    // let classeStatus = ''
+    // let statusStringCor = ''
+    // if (status == 1){
+    //     classeStatus = 'text-lime-500'
+    //     statusStringCor = 'verde'
+    // } else if (status == 2){
+    //     classeStatus = 'text-amber-300'
+    //     statusStringCor = 'amarelo'
+    // } else if (status == 3) {
+    //     classeStatus = 'text-amber-500'
+    //     statusStringCor = 'laranja'
+    // } else {
+    //     classeStatus = 'text-red-500'
+    //     statusStringCor = 'vermelho'
+    // }
 
-    function unixConverter(timestamp: number | undefined){
-        if (timestamp == undefined){
-            return ''
-        }
-        let currentDate = new Date(timestamp)
-        let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-        let year = String(currentDate.getFullYear());
-        let month= String(months[currentDate.getMonth()]);
-        let date = String(currentDate.getDate());
-        if (date.length<2) {
-            date = '0'+date
-        }
-        let recordDate = date+'/'+month+'/'+year
-        return recordDate
-    }
+    // function unixConverter(timestamp: number | undefined){
+    //     if (timestamp == undefined){
+    //         return ''
+    //     }
+    //     let currentDate = new Date(timestamp)
+    //     let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+    //     let year = String(currentDate.getFullYear());
+    //     let month= String(months[currentDate.getMonth()]);
+    //     let date = String(currentDate.getDate());
+    //     if (date.length<2) {
+    //         date = '0'+date
+    //     }
+    //     let recordDate = date+'/'+month+'/'+year
+    //     return recordDate
+    // }
 
-    let dia = unixConverter(data?.[0]?.data_iniSE)
-    let casos = data?.[0]?.casos
+    // let dia = unixConverter(data?.[0]?.data_iniSE)
+    // let casos = data?.[0]?.casos
 
     return (
         <>
@@ -90,7 +90,7 @@ export default function Atualmente() {
             borderStyle:'solid',
             borderWidth:'thin',
         }}>
-            <h2 className="text-2xl">Veja os dados sobre a dengue em Indaiatuba na última semana:</h2>
+            {/* <h2 className="text-2xl">Veja os dados sobre a dengue em Indaiatuba na última semana:</h2>
             {loading && <h2>Carregando...</h2>}
             {error && <p>Sentimos muito, ocorreu um erro ao recuperar os dados</p>}
             {data && !loading && !error && (
@@ -98,7 +98,7 @@ export default function Atualmente() {
             <p>Indaiatuba teve {casos} casos de dengue essa semana ({dia})</p>
             <p className={classeStatus}>O nível de alerta é {statusStringCor}!</p>
             </>
-            )}
+            )} */}
         </div>
         <Footer />
         </>
--- src/app/matematica/page.tsx
+++ src/app/matematica/page.tsx
@@ -1,8 +1,26 @@
 'use client'
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import Titulo from "@/components/titulo";
+import Artigo from "@/components/artigo";
 
 export default function Matematica() {
     return (
+        <>
         <Navbar/>
+        <Titulo texto="Modelo SIR"></Titulo>
+        <Artigo 
+        texto="O modelo SIR é um dos mais básicos modelos matemáticos que visam prever as infecções por uma doença ao longo do tempo, este busca descrever a transição de membros da população total N pelos grupos S (suscetíveis), I (infectados) e R (removidos), que acontece em função da taxa de infecção β (infectados/tempo) e a taxa de recuperação γ (recuperados/tempo). Esse modelo assume o desenvolvimento de imunidade pelo indivíduo após a infecção, o que é aplicável para um único sorotipo da dengue, porém devido a existência de quatro sorotipos conhecidos da doença, ele é incapaz de levar em conta reinfecções por diferentes sorotipos e representar a enfermidade como um todo."
+        posicaoDoTexto="R"
+        nomeImagem="ilust_sir.png"
+        altImagem="Ilustração de três caixas denominadas S (suscetíveis), I (infectados) e R (removidos), setas indicam um fluxo unidirecional entre elas." />
+        <Titulo texto="Modelo de Newton e Reiter" />
+        <Artigo
+        texto='Para o estudo da relação entre a dengue e a população humana, um dos modelos matemáticos que pode ser utilizado é o de Newton e Reiter de 1992. Este modelo permite que sejam analisadas as duas principais formas de infecção da doença: a forma clássica (não-letal) e a forma hemorrágica (potencialmente letal), analisando a taxa de infecção entre a população humana e a população de mosquitos, levando algumas variáveis em consideração. Alguns exemplos das variáveis utilizadas no método de Newton e Reiter são o tamanho das populações, as taxas migratórias (migração e imigração) e as taxas vitais (nascimentos e mortes). Já na análise da população de mosquitos, pode ser considerado um crescimento lógico da população. Deve se levar em consideração que a dengue é uma doença sazonal, que ocorre principalmente durante o verão ou em períodos de chuvas intensas. A utilização do método de Newton e Reiter no estudo da dengue demonstra que tais métodos tendem a ajudar muito na compreensão dos avanços epidémicos ocasionados pela doença. Tais cenários, apesar de serem hipotéticos, auxiliam muito na compreensão e na tomada de decisão de medidas preventivas para a contenção do quadro epidemiológico em uma determinada região.'
+        posicaoDoTexto="L"
+        nomeImagem="grafico_newton_e_reiter.png"
+        altImagem="Gráfico de Ana Paula P. Wyse, Amaury Oliveira Gemaque e Guzman Eulálio Isla Chamilco, que demonstra o número de indivíduos infectados pelas dengues clássica e hemorrágica, pode-se ver que ambos crescem exponencialmente até um pico na metade do gráfico após 90 dias, e em seguida diminuem até zero no mesmo ritmo em que antes cresciam" />
+        <Footer />
+        </>
     )
 }
