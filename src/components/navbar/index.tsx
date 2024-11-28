f128d8e348ea01f4e26409198a76863e2315d6db
ericmeneg <ericvinicius.menegon@gmail.com>
2024-11-28 16:02:57 -0300

    Removendo imports desnecessários para evitar erros de build

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,26 +4,12 @@ import { useState } from 'react'
 import Link from 'next/link'
 import {
   Dialog,
-  DialogPanel,
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
+  DialogPanel
 } from '@headlessui/react'
 import {
-  ArrowPathIcon,
   Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
