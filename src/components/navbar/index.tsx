d54e1dd8587c5fc1324d05f58e6fade742465ab8
ericmeneg <ericvinicius.menegon@gmail.com>
2024-11-28 15:59:22 -0300

    adicionando navbar responsiva

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,18 +1,101 @@
-import Link from "next/link"
+'use client'
+
+import { useState } from 'react'
+import Link from 'next/link'
+import {
+  Dialog,
+  DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
+  PopoverGroup,
+  PopoverPanel,
+} from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 
 export default function Navbar() {
-    return (
-        <div className="bg-white flex flex-col px-0">
-            <nav className="flex justify-center bg-green-200 p-3">
-                <Link href="/" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">Home</Link>
-                <Link href="/login" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">Login</Link>
-                <Link href="/oque" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">O que é?</Link>
-                <Link href="/historico" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">Histórico</Link>
-                <Link href="/atualmente" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">Atualmente</Link>
-                <Link href="/diagnostico" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">Diagnóstico</Link>
-                <Link href="/prevencao" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">Prevenção e Tratamento</Link>
-                <Link href="/matematica" className="mx-4 px-4 py-2 font-semibold text-black hover:bg-green-300 rounded-md">Modelos</Link>
-            </nav>
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <header className="bg-green-200">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              alt=""
+              src="/images/home-svgrepo-com.svg"
+              className="h-8 w-auto"
+            />
+          </Link>
         </div>
-    )
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        {/* Links for desktop */}
+        <div className="hidden lg:flex lg:gap-x-12">
+                <Link href="/" className="linkNavBar">Home</Link>
+                <Link href="/login" className="linkNavBar">Login</Link>
+                <Link href="/oque" className="linkNavBar">O que é?</Link>
+                <Link href="/historico" className="linkNavBar">Histórico</Link>
+                <Link href="/atualmente" className="linkNavBar">Atualmente</Link>
+                <Link href="/diagnostico" className="linkNavBar">Sintomas e Diagnóstico</Link>
+                <Link href="/prevencao" className="linkNavBar">Prevenção e Tratamento</Link>
+                <Link href="/matematica" className="linkNavBar">Modelos</Link>
+        </div>
+      </nav>
+      {/* Mobile menu */}
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-green-200 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="/images/home-svgrepo-com.svg"
+                className="h-8 w-auto"
+              />
+            </Link>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 space-y-2">
+                <Link href="/" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">Home</Link>
+                <Link href="/login" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">Login</Link>
+                <Link href="/oque" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">O que é?</Link>
+                <Link href="/historico" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">Histórico</Link>
+                <Link href="/atualmente" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">Atualmente</Link>
+                <Link href="/diagnostico" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">Sintomas e Diagnóstico</Link>
+                <Link href="/prevencao" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">Prevenção e Tratamento</Link>
+                <Link href="/matematica" className="linkNavBarMobile block rounded-lg py-2 px-3 text-base font-semibold text-gray-900 hover:bg-green-300">Modelos</Link>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  )
 }
